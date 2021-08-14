45e15802af8aa06ae66f4b079cd0bb4160679331
Rickyose <rickyhaterearged@gmail.com>
2021-08-14 23:12:49 +0700

    Update buru2.sh

--- buru2.sh
+++ buru2.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 #by pudh
 
-sudo ufw default deny incoming
-sudo ufw allow from 203.0.113.4
+sudo su root
+# flush all chains
+iptables -F
+iptables -t nat -F
+iptables -t mangle -F
+# delete all chains
+iptables -X
+
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT 
+iptables -A INPUT -p icmp -m icmp --icmp-type 11 -j ACCEPT
+iptables -A INPUT -p icmp -m icmp --icmp-type 3/4 -j ACCEPT 
+iptables -A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT
+iptables -I INPUT -p tcp -s  208.53.12.223 --dport 5901 -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -j REJECT --reject-with icmp-host-unreachable

7eb39a057b0f1799d52d2c0d2d24c4383ff67510
Rickyose <rickyhaterearged@gmail.com>
2021-08-15 00:46:36 +0700

    Update buru2.sh

--- buru2.sh
+++ buru2.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #by pudh
 
-sudo su root
-
+root -l <<-EOF
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT 
 iptables -A INPUT -p icmp -m icmp --icmp-type 11 -j ACCEPT
@@ -11,3 +10,4 @@ iptables -A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT
 iptables -I INPUT -p tcp -s  208.53.12.223 --dport 5901 -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -j REJECT --reject-with icmp-host-unreachable
+EOF

2d69ff6768860ef10da9d488ecf4f21ab7191b1f
Rickyose <rickyhaterearged@gmail.com>
2021-08-15 03:04:07 +0700

    Update buru2.sh

--- buru2.sh
+++ buru2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #by pudh
 
+######################################################################## IP TABLES ####################################################################################
 #### Agar tidak terlocked 
 sudo -u root iptables -P INPUT ACCEPT
 sudo -u root iptables -P FORWARD ACCEPT
@@ -19,5 +20,11 @@ sudo -u root iptables -A INPUT -p icmp -m icmp --icmp-type 3/4 -j ACCEPT
 sudo -u root iptables -A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT
 sudo -u root iptables -I INPUT -p tcp -s  125.163.173.171 --dport 5901 -j ACCEPT
 sudo -u root iptables -I INPUT -p tcp -s  208.53.12.223 --dport 5901 -j ACCEPT
-sudo -u root iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-sudo -u root iptables -A INPUT -p tcp --dport 5901 -j REJECT --reject-with icmp-host-unreachable
+sudo -u root iptables -A INPUT -p tcp --dport 22 -j ACCEPT #untuk ssh
+sudo -u root iptables -A INPUT -p tcp --dport 8000 -j ACCEPT #untuk sharing folder ke internet
+sudo -u root iptables -A INPUT -p tcp --dport 8080 -j ACCEPT #untuk rclone
+sudo -u root iptables -A INPUT -p tcp --dport 443 -j ACCEPT #untuk rclone
+sudo -u root iptables -A INPUT -p tcp --dport 5901 -j ACCEPT  #untuk reject vnc 
+sudo -u root iptables -A INPUT -j REJECT --reject-with icmp-host-unreachable
+
+#######################################################################################################################################################################

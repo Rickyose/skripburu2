5371c0e77be1ae2fd1177d6efe0dd6844bc2ee38
Rickyose <rickyhaterearged@gmail.com>
2021-08-15 00:49:51 +0700

    Update buru2.sh

--- buru2.sh
+++ buru2.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #by pudh
 
-root -l <<-EOF
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT 
-iptables -A INPUT -p icmp -m icmp --icmp-type 11 -j ACCEPT
-iptables -A INPUT -p icmp -m icmp --icmp-type 3/4 -j ACCEPT 
-iptables -A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT
-iptables -I INPUT -p tcp -s  208.53.12.223 --dport 5901 -j ACCEPT
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-iptables -A INPUT -j REJECT --reject-with icmp-host-unreachable
-EOF
+
+sudo -u root iptables -A INPUT -i lo -j ACCEPT
+sudo -u root iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT 
+sudo -u root iptables -A INPUT -p icmp -m icmp --icmp-type 11 -j ACCEPT
+sudo -u root iptables -A INPUT -p icmp -m icmp --icmp-type 3/4 -j ACCEPT 
+sudo -u root iptables -A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT
+sudo -u root iptables -I INPUT -p tcp -s  208.53.12.223 --dport 5901 -j ACCEPT
+sudo -u root iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo -u root iptables -A INPUT -j REJECT --reject-with icmp-host-unreachable

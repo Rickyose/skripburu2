99305be48b83adf727ceb3f8da244ca1078af7d0
Rickyose <rickyhaterearged@gmail.com>
2021-10-04 20:48:06 +0700

    Update buru2.sh

--- buru2.sh
+++ buru2.sh
@@ -3,30 +3,30 @@
 
 ######################################################################## IP TABLES ####################################################################################
 #### Agar tidak terlocked 
-#sudo -u root iptables -P INPUT ACCEPT
-#sudo -u root iptables -P FORWARD ACCEPT
-#sudo -u root iptables -P OUTPUT ACCEPT
+sudo -u root iptables -P INPUT ACCEPT
+sudo -u root iptables -P FORWARD ACCEPT
+sudo -u root iptables -P OUTPUT ACCEPT
 
 ############ Flush 
-#sudo -u root iptables -t nat -F
-#sudo -u root iptables -t mangle -F
-#sudo -u root iptables -F
-#sudo -u root iptables -X
+sudo -u root iptables -t nat -F
+sudo -u root iptables -t mangle -F
+sudo -u root iptables -F
+sudo -u root iptables -X
 
 
-#sudo -u root iptables -I INPUT -p tcp -s  125.163.173.171 --dport 5901 -j ACCEPT
-#sudo -u root iptables -I INPUT -p tcp -s  125.163.172.125 --dport 5901 -j ACCEPT
-#sudo -u root iptables -I INPUT -p tcp -s  195.201.113.42 --dport 5901 -j ACCEPT # IP VPS Untuk monitoring
-#sudo -u root iptables -I INPUT -p tcp -s  159.69.152.161 --dport 5901 -j ACCEPT # IP VPS Untuk monitoring
-#sudo -u root iptables -I INPUT -p tcp -s  159.69.153.127 --dport 5901 -j ACCEPT # IP VPS Untuk monitoring
-#sudo -u root iptables -I INPUT -p tcp -s  65.21.1.39 --dport 5901 -j ACCEPT # IP VPS Untuk monitoring
-#sudo -u root iptables -I INPUT -p tcp -s  208.53.12.223 --dport 5901 -j ACCEPT
-#sudo -u root iptables -I INPUT -p tcp -s  36.72.217.240 --dport 5901 -j ACCEPT
-#sudo -u root iptables -I INPUT -p tcp -s  180.254.26.167 --dport 5901 -j ACCEPT
-#sudo -u root iptables -A INPUT -p tcp --dport 22 -j ACCEPT #untuk ssh
-#sudo -u root iptables -A INPUT -p tcp --dport 8000 -j ACCEPT #untuk sharing folder ke internet
-#sudo -u root iptables -A INPUT -p tcp --dport 8080 -j ACCEPT #untuk rclone
-#sudo -u root iptables -A INPUT -p tcp --dport 443 -j ACCEPT #untuk rclone
-#sudo -u root iptables -A INPUT -p tcp -s 0.0.0.0/0 --dport 5901 -j DROP #untuk reject vnc 
+sudo -u root iptables -I INPUT -p tcp -s  125.163.173.171 --dport 5901 -j ACCEPT
+sudo -u root iptables -I INPUT -p tcp -s  125.163.172.125 --dport 5901 -j ACCEPT
+sudo -u root iptables -I INPUT -p tcp -s  195.201.113.42 --dport 5901 -j ACCEPT # IP VPS Untuk monitoring
+sudo -u root iptables -I INPUT -p tcp -s  159.69.152.161 --dport 5901 -j ACCEPT # IP VPS Untuk monitoring
+sudo -u root iptables -I INPUT -p tcp -s  159.69.153.127 --dport 5901 -j ACCEPT # IP VPS Untuk monitoring
+sudo -u root iptables -I INPUT -p tcp -s  65.21.1.39 --dport 5901 -j ACCEPT # IP VPS Untuk monitoring
+sudo -u root iptables -I INPUT -p tcp -s  208.53.12.223 --dport 5901 -j ACCEPT
+sudo -u root iptables -I INPUT -p tcp -s  36.72.217.240 --dport 5901 -j ACCEPT
+sudo -u root iptables -I INPUT -p tcp -s  180.254.26.167 --dport 5901 -j ACCEPT
+sudo -u root iptables -A INPUT -p tcp --dport 22 -j ACCEPT #untuk ssh
+sudo -u root iptables -A INPUT -p tcp --dport 8000 -j ACCEPT #untuk sharing folder ke internet
+sudo -u root iptables -A INPUT -p tcp --dport 8080 -j ACCEPT #untuk rclone
+sudo -u root iptables -A INPUT -p tcp --dport 443 -j ACCEPT #untuk rclone
+sudo -u root iptables -A INPUT -p tcp -s 0.0.0.0/0 --dport 5901 -j DROP #untuk reject vnc 
 
 #######################################################################################################################################################################

44410002678b5c074775121f06ffa3549886b642
Rickyose <rickyhaterearged@gmail.com>
2021-10-23 20:15:32 +0700

    Update check.sh

--- check.sh
+++ check.sh
@@ -3,6 +3,7 @@
 sudo apt install vnstat -y
 sudo systemctl enable vnstat.service
 
+export  oracle_user=`cat /etc/apt/oracle_user.txt | sed -n "$1"P`
 ip=`curl ifconfig.me`
 jumlah_vcpu=`cat /proc/cpuinfo | grep processor | wc -l`
 jenis_cpu=`uname -m`
@@ -13,7 +14,7 @@ daily_total=`sudo vnstat --oneline |  awk '{print $3}' | awk -F";" '{ print $NF 
 daily_total_satuan=`sudo vnstat --oneline |  awk '{print $4}' | awk -F";" '{print $1}'`
 
 pre_message="$@======================================================="
-message="$@ IP = $ip | vCPU = $jumlah_vcpu | Arm/AMD = $jenis_cpu | Uptime = $how_long_uptime | Kec/daily = $daily_average $daily_average_satuan | Total Upload/daily = $daily_total $daily_total_satuan"
+message="$@ Oracle Acc = $oracle_user | IP = $ip | vCPU = $jumlah_vcpu | Arm/AMD = $jenis_cpu | Uptime = $how_long_uptime | Kec/daily = $daily_average $daily_average_satuan | Total Upload/daily = $daily_total $daily_total_satuan"
 
 ## format to parse to curl
 ## echo Sending message: $message

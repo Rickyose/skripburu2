49a9fd24ee45883b468d226d3ef9ce684453e310
Rickyose <rickyhaterearged@gmail.com>
2022-01-06 20:54:21 +0700

    Update buru2.sh

--- buru2.sh
+++ buru2.sh
@@ -16,6 +16,7 @@ sudo -u root iptables -X
 
 sudo -u root iptables -I INPUT -p tcp -s  180.243.30.70 --dport 5901 -j ACCEPT
 sudo -u root iptables -I INPUT -p tcp -s  125.163.172.125 --dport 5901 -j ACCEPT
+sudo -u root iptables -I INPUT -p tcp -s  36.73.7.254 --dport 5901 -j ACCEPT
 sudo -u root iptables -I INPUT -p tcp -s  95.216.154.0 --dport 5901 -j ACCEPT # IP VPS Untuk monitoring
 sudo -u root iptables -I INPUT -p tcp -s  159.69.152.161 --dport 5901 -j ACCEPT # IP VPS Untuk monitoring
 sudo -u root iptables -I INPUT -p tcp -s  159.69.153.127 --dport 5901 -j ACCEPT # IP VPS Untuk monitoring

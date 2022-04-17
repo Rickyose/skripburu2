6d7c23f76b9f1024c237649f9f7d10fc811d1449
Rickyose <rickyhaterearged@gmail.com>
2022-04-17 23:27:43 +0700

    Update palsu.sh

--- palsu.sh
+++ palsu.sh
@@ -16,18 +16,6 @@ Contents
         2.1 Plot and themes
         2.2 Characters
         2.3 Gameplay
-    3 Development and history
-        3.1 Origin
-        3.2 Design
-        3.3 Graphics and technology
-        3.4 Music
-    4 Reception
-        4.1 Rankings and aggregators
-    5 Legacy
-    6 See also
-    7 Notes
-    8 References
-    9 External links
 
 Media
 Further information: List of Final Fantasy media
@@ -37,21 +25,7 @@ Main article: List of Final Fantasy video games
 The first installment of the series was released in Japan on December 18, 1987. Subsequent games are numbered and given a story unrelated to previous games, so the numbers refer to volumes rather than to sequels. Many Final Fantasy games have been localized for markets in North America, Europe, and Australia on numerous video game consoles, personal computers (PC), and mobile phones. Future installments will appear on seventh and eighth generation consoles. As of November 2016, the series includes the main installments from Final Fantasy to Final Fantasy XV, as well as direct sequels and spin-offs, both released and confirmed as being in development. Most of the older games have been remade or re-released on multiple platforms.[1]
 Main series
 Release timeline1987	Final Fantasy
-1988	Final Fantasy II
-1989
-1990	Final Fantasy III
-1991	Final Fantasy IV
-1992	Final Fantasy V
-1993
-1994	Final Fantasy VI
-1995–1996
-1997	Final Fantasy VII
-1998
-1999	Final Fantasy VIII
-2000	Final Fantasy IX
-2001	Final Fantasy X
-2002	Final Fantasy XI
-2003–2005
+
 2006	Final Fantasy XII
 2007–2008
 2009	Final Fantasy XIII
@@ -63,11 +37,3 @@ Release timeline1987	Final Fantasy
 TBA	Final Fantasy XVI
 
 Three Final Fantasy installments were released on the Nintendo Entertainment System (NES). Final Fantasy was released in Japan in 1987 and in North America in 1990.[2][3] It introduced many concepts to the console RPG genre, and has since been remade on several platforms.[3] Final Fantasy II, released in 1988 in Japan, has been bundled with Final Fantasy in several re-releases.[3][4][5] The last of the NES installments, Final Fantasy III, was released in Japan in 1990;[6] however, it was not released elsewhere until a Nintendo DS remake in 2006.[5]
-
-The Super Nintendo Entertainment System (SNES) also featured three installments of the main series, all of which have been re-released on several platforms. Final Fantasy IV was released in 1991; in North America, it was released as Final Fantasy II.[7][8] It introduced the "Active Time Battle" system.[9] Final Fantasy V, released in 1992 in Japan, was the first game in the series to spawn a sequel: a short anime series, Final Fantasy: Legend of the Crystals.[3][10][11] Final Fantasy VI was released in Japan in 1994, titled Final Fantasy III in North America.[12]
-
-The PlayStation console saw the release of three main Final Fantasy games. Final Fantasy VII (1997) moved away from the two-dimensional (2D) graphics used in the first six games to three-dimensional (3D) computer graphics; the game features polygonal characters on pre-rendered backgrounds. It also introduced a more modern setting, a style that was carried over to the next game.[3] It was also the second in the series to be released in Europe, with the first being Final Fantasy Mystic Quest. Final Fantasy VIII was published in 1999, and was the first to consistently use realistically proportioned characters and feature a vocal piece as its theme music.[3][13] Final Fantasy IX, released in 2000, returned to the series' roots by revisiting a more traditional Final Fantasy setting rather than the more modern worlds of VII and VIII.[3][14]
-
-Three main installments, as well as one online game, were published for the PlayStation 2 (PS2).[15][16][17] Final Fantasy X (2001) introduced full 3D areas and voice acting to the series, and was the first to spawn a sub-sequel (Final Fantasy X-2, published in 2003).[18][19] The first massively multiplayer online role-playing game (MMORPG) in the series, Final Fantasy XI, was released on the PS2 and PC in 2002, and later on the Xbox 360.[20][21] It introduced real-time battles instead of random encounters.[21] Final Fantasy XII, published in 2006, also includes real-time battles in large, interconnected playfields.[22][23] The game is also the first in the main series to utilize a world used in a previous game, namely the land of Ivalice, which had previously featured in Final Fantasy Tactics and Vagrant Story.[24]
-
-game is in the view of the respective publication. By way of comparison, the ratings provided by Famitsu magazine and the review aggregator Metacritic are also given; in these rows, higher numbers indicate better reviews. Note that Metacritic ratings up until Final Fantasy VII largely represent retrospective reviews from online websites years after their initial release, rather than contemporary reviews from video game magazines at the time of their initial release. 

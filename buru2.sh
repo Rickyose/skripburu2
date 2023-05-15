30b7f2a2f11bbf2967ec989fbbe893e96b9456b0
Rickyose <rickyhaterearged@gmail.com>
2023-05-15 17:17:08 +0700

    Update buru2.sh

--- buru2.sh
+++ buru2.sh
@@ -14,18 +14,7 @@ sudo -u root iptables -F
 sudo -u root iptables -X
 
 sudo -u root iptables -I INPUT -p tcp -s  5.161.52.191 --dport 5901 -j ACCEPT # IP PROXY Untuk monitoring
-sudo -u root iptables -I INPUT -p tcp -s  140.213.163.63 --dport 5901 -j ACCEPT # IP VPS Untuk monitoring
-sudo -u root iptables -I INPUT -p tcp -s  180.243.30.70 --dport 5901 -j ACCEPT
-sudo -u root iptables -I INPUT -p tcp -s  125.163.172.125 --dport 5901 -j ACCEPT
-sudo -u root iptables -I INPUT -p tcp -s  36.73.7.254 --dport 5901 -j ACCEPT
-sudo -u root iptables -I INPUT -p tcp -s  95.216.154.0 --dport 5901 -j ACCEPT # IP VPS Untuk monitoring
-sudo -u root iptables -I INPUT -p tcp -s  65.108.154.38 --dport 5901 -j ACCEPT # IP VPS Untuk monitoring
-sudo -u root iptables -I INPUT -p tcp -s  159.69.153.127 --dport 5901 -j ACCEPT # IP VPS Untuk monitoring
-sudo -u root iptables -I INPUT -p tcp -s  65.21.1.39 --dport 5901 -j ACCEPT # IP VPS Untuk monitoring
-sudo -u root iptables -I INPUT -p tcp -s  125.166.229.14 --dport 5901 -j ACCEPT
-sudo -u root iptables -I INPUT -p tcp -s  208.53.12.223 --dport 5901 -j ACCEPT
-sudo -u root iptables -I INPUT -p tcp -s  36.72.217.240 --dport 5901 -j ACCEPT
-sudo -u root iptables -I INPUT -p tcp -s  180.254.26.167 --dport 5901 -j ACCEPT
+sudo -u root iptables -I INPUT -p tcp -s  116.203.220.174 --dport 5901 -j ACCEPT # IP VPS Untuk monitoring
 sudo -u root iptables -A INPUT -p tcp --dport 22 -j ACCEPT #untuk ssh
 sudo -u root iptables -A INPUT -p tcp --dport 8000 -j ACCEPT #untuk sharing folder ke internet
 sudo -u root iptables -A INPUT -p tcp --dport 8080 -j ACCEPT #untuk rclone

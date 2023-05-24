e305ec656a2f2356bc7738d8f2f9c038ae58035f
Rickyose <rickyhaterearged@gmail.com>
2023-05-25 06:00:46 +0700

    Update buru2.sh

--- buru2.sh
+++ buru2.sh
@@ -13,7 +13,7 @@ sudo -u root iptables -t mangle -F
 sudo -u root iptables -F
 sudo -u root iptables -X
 
-sudo -u root iptables -I INPUT -p tcp -s  5.161.52.191 --dport 5901 -j ACCEPT # IP PROXY Untuk monitoring
+sudo -u root iptables -I INPUT -p tcp -s  180.253.130.185  --dport 5901 -j ACCEPT # IP PROXY Untuk monitoring
 sudo -u root iptables -I INPUT -p tcp -s  116.203.220.174 --dport 5901 -j ACCEPT # IP VPS Untuk monitoring
 sudo -u root iptables -A INPUT -p tcp --dport 22 -j ACCEPT #untuk ssh
 sudo -u root iptables -A INPUT -p tcp --dport 8000 -j ACCEPT #untuk sharing folder ke internet
